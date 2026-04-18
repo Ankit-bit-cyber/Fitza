f4b916a2533bd5530a316950380a3f0f4a4d12f9
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-04-19 00:50:40 +0530

    refactor: remove unused CartPage and ProductPage components

--- frontend/src/pages/CartPage.jsx
+++
@@ -1,8 +0,0 @@
-// Cart Page
-import React from 'react';
-
-const CartPage = () => {
-  return <h1>Your Cart</h1>;
-};
-
-export default CartPage;
--- frontend/src/pages/ProductPage.jsx
+++
@@ -1,8 +0,0 @@
-// Product Page
-import React from 'react';
-
-const ProductPage = () => {
-  return <h1>Product Details</h1>;
-};
-
-export default ProductPage;
