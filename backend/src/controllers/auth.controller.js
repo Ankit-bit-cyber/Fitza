512d5495d08b0f5a727b0bf44e824c087b9ed1fa
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-04-19 00:52:41 +0530

    feat: implement authentication controller with register, login, and getProfile functionalities

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,27 @@
 // Authentication Controller
-exports.login = (req, res) => {
-  res.send('Login functionality');
+auth.controller.js
+const authService = require('../services/auth.service');
+const { successResponse } = require('../utils/helpers');
+
+const register = async (req, res, next) => {
+  try {
+    const result = await authService.register(req.body);
+    return successResponse(res, result, 'Registration successful.', 201);
+  } catch (err) { next(err); }
 };
 
-exports.register = (req, res) => {
-  res.send('Register functionality');
+const login = async (req, res, next) => {
+  try {
+    const result = await authService.login(req.body);
+    return successResponse(res, result, 'Login successful.');
+  } catch (err) { next(err); }
 };
+
+const getProfile = async (req, res, next) => {
+  try {
+    const user = await authService.getProfile(req.user._id);
+    return successResponse(res, { user });
+  } catch (err) { next(err); }
+};
+
+module.exports = { register, login, getProfile };
