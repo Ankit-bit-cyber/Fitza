939bf9d689dc94f330bf074c26a123db478822ae
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-04-19 00:55:30 +0530

    feat: implement product controller with getProducts, getProductById, and getFilterOptions functionalities

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,8 +1,25 @@
-// Product Controller
-exports.getProducts = (req, res) => {
-  res.send('Get all products');
+const productService = require('../services/product.service');
+const { successResponse, paginatedResponse } = require('../utils/helpers');
+
+const getProducts = async (req, res, next) => {
+  try {
+    const { products, total, page, limit } = await productService.getProducts(req.query);
+    return paginatedResponse(res, products, total, page, limit);
+  } catch (err) { next(err); }
 };
 
-exports.getProductById = (req, res) => {
-  res.send('Get product by ID');
+const getProductById = async (req, res, next) => {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    return successResponse(res, { product });
+  } catch (err) { next(err); }
 };
+
+const getFilterOptions = async (req, res, next) => {
+  try {
+    const options = await productService.getFilterOptions();
+    return successResponse(res, options);
+  } catch (err) { next(err); }
+};
+
+module.exports = { getProducts, getProductById, getFilterOptions };
